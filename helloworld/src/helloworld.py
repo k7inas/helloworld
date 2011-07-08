9af302bddf3b055b220ffe5b78437085b096f3f1
k7inas@163.com <k7inas@163.com>
2011-07-08 11:35:52 +0800

    first commit

---
+++ helloworld/src/helloworld.py
@@ -0,0 +1,19 @@
+'''
+Created on 2011-6-15
+
+@author: k7inas
+'''
+'''
+x = int(input("enter an integer: "))
+if x<0:
+    x = 0
+    print ("change to zero")
+elif x==0:
+    print ("zero")
+else:
+    print("more")
+'''
+
+a = ['cat', 'dog', 'pig']
+for z in a:
+    print (z, len(z))
